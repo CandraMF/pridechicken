926b42ef4a7db2ddd3a985d8bf991d9ace5aab54
CandraMF <candragain99@gmail.com>
2024-01-10 10:26:27 +0700

    navbar

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import MainLayout from '@/Layouts/MainLayout';
+import { Head } from '@inertiajs/react';
+
+export default function Home() {
+    return (
+        <MainLayout>
+            <Head title="Home" />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">ASdasd</div>
+                    </div>
+                </div>
+            </div>
+        </MainLayout>
+    );
+}
