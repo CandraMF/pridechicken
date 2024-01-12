21f360d86363f0ceea329ef9efda1c3c5a9a9090
CandraMF <candragain99@gmail.com>
2024-01-12 11:06:45 +0700

    init

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,12 @@
+import Checkbox from '@/Components/Checkbox';
+import TextInput from '@/Components/TextInput';
 import MainLayout from '@/Layouts/MainLayout';
 import { Head } from '@inertiajs/react';
-import { Carousel, Button } from 'flowbite-react';
+import { Carousel, Button, Card, Avatar, Label, Radio, Textarea, FileInput } from 'flowbite-react';
 import { useState } from 'react';
+
+import { MdEmail, MdPhone, MdLocationOn } from "react-icons/md";
+
 
 const Home = () => {
 
@@ -53,6 +58,58 @@ const Home = () => {
             answer: "Bahan baku wajib tersebut adalah ayam, tepung, kemasan branding, dan saus pilihan.",
             action: false,
             actionTitle: "",
+        },
+    ]
+
+    const comments = [
+        {
+            name: "Pak Putra",
+            address: "Semarang",
+            photo: "https://cdn.antaranews.com/cache/1200x800/2021/11/06/Andika-Perkasa.jpg",
+            comment: "Rata-rata per hari bisa habis 40-50 ekor ayam dengan omzet mencapai 100 juta (perbulan), alhamdulillah.."
+        },
+        {
+            name: "Pak Jefri",
+            address: "Bandung",
+            photo: "https://cdn.antaranews.com/cache/1200x800/2021/11/06/Andika-Perkasa.jpg",
+            comment: "Alhamdulillah.. sehari bisa menjual 30 ekor ayam dan omzet perbulan bisa mencapai Rp.90.000.000."
+        },
+        {
+            name: "Pak Hasan",
+            address: "Riau",
+            photo: "https://cdn.antaranews.com/cache/1200x800/2021/11/06/Andika-Perkasa.jpg",
+            comment: "Per hari mampu menjual 100 ekor ayam dengan omsetnya mencapai 300 jutaan per bulan! MasyaAllah!"
+        },
+    ]
+
+    const steps = [
+        {
+            title: "Hubungi Pride Chicken",
+            desc: "Mitra menghubungi pride chicken di 0881 - 0818 - 49988",
+        },
+        {
+            title: "Pilih Paket Kemitraan Dan Isi Formulir",
+            desc: "Pilih paket kemitraan",
+        },
+        {
+            title: "Shareloc & Foto Lokasi",
+            desc: "Mitra wajib melampirkan shareloc dan foto via wa",
+        },
+        {
+            title: "Survey",
+            desc: "Tim manajemen mendatangi lokasi dan survey",
+        },
+        {
+            title: "Training & Test",
+            desc: "Mitra wajib mengikuti training operasional sambil menunggu booth dikirim ke lokasi berjualan",
+        },
+        {
+            title: "Kirim Booth",
+            desc: "Pride Chicken akan mengirimkan booth ke lokasi mitra",
+        },
+        {
+            title: "Siap Berjualan",
+            desc: "Mitra Siap Berjualan",
         },
     ]
 
@@ -297,12 +354,12 @@ const Home = () => {
             </div>
 
             <div className="min-h-screen max-w-6xl mx-auto px-6 lg:px-8">
-                <div className="grid grid-cols-2 gap-10 justify-center h-screen">
+                <div className="grid lg:grid-cols-2 grid-cols-1 gap-10 justify-center md:py-0 py-20 min-h-screen">
                     <div className="flex justify-center items-center h-full">
-                        <img className='h-2/3' src="assets/images/booth/booth roda.png" alt="" />
+                        <img className='md:h-2/3' src="assets/images/booth/booth roda.png" alt="" />
                     </div>
                     <div className='flex justify-start items-center h-full text-white'>
-                        <div>
+                        <div className=''>
                             <h2 className='text-4xl font-semibold'>Keuntungan Bermitra dengan Pride Chicken</h2>
                             <ul className="list-disc mt-5 pl-5">
                                 <li>Free Royalti</li>
@@ -324,12 +381,12 @@ const Home = () => {
             </div>
 
             <div className="min-h-screen max-w-6xl mx-auto px-6 lg:px-8 pb-10">
-                <div className="grid grid-cols-2 gap-10 justify-center h-screen">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-10 justify-center min-h-screen">
                     <div className='flex justify-start items-center h-full text-white'>
                         <div>
-                            <h2 className='text-4xl font-semibold'># FAQ</h2>
+                            <h2 className='text-4xl font-semibold md:text-start text-center'># FAQ</h2>
 
-                            <div className='flex flex-col gap-3 items-start mt-10'>
+                            <div className='flex flex-col gap-3 items-center md:items-start mt-10'>
                                 {faqs.map((faq, index) => {
                                     console.log(faq.question)
                                     return (
@@ -343,14 +400,171 @@ const Home = () => {
                         <div className="bg-white w-full min-h-80 p-8 rounded-xl">
                             <h2 className='text-2xl font-semibold text-[#FAA41A]'>Jawab</h2>
                             <p className='my-3'>{faqs[selectTab].answer}</p>
-                            
                             {faqs[selectTab].action && <Button className='mt-5' color="failure" outline>
                                 {faqs[selectTab].actionTitle}
                             </Button>}
-
                         </div>
                     </div>
 
+                </div>
+            </div>
+
+            <div className="min-h-screen max-w-6xl mx-auto px-6 lg:px-8 pb-10">
+
+                <div className='flex flex-col justify-center items-center h-full text-white min-h-screen'>
+                    <h2 className='text-4xl font-semibold md:text-start text-center mb-10'>Kata Mitra Pride Chicken</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-5 justify-center">
+                        {comments.map((value, index) => {
+                            return (
+                                <Card href="#" className="max-w-sm">
+                                    <Avatar img={value.photo} rounded size="xl"></Avatar>
+                                    <h5 className="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">
+                                        {value.name}
+                                    </h5>
+                                    <p className="font-normal text-center text-gray-700 dark:text-gray-400">
+                                        Pemilik Outlet {value.address}
+                                    </p>
+                                    <p className="font-normal text-center text-gray-700 dark:text-gray-400">
+                                        "{value.comment}"
+                                    </p>
+                                </Card>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+
+            <div className="min-h-screen max-w-6xl mx-auto px-6 lg:px-8 pb-10">
+
+                <div className='flex flex-col justify-center items-center h-full text-white min-h-screen'>
+                    <h2 className='text-4xl font-semibold md:text-start text-center mb-10'># Langkah Gabung Menjadi Mitra</h2>
+                    <div className="grid w-full grid-rows-1 md:grid-rows-4 md:grid-flow-col grid-flow-row gap-x-10 gap-y-5">
+
+                        {steps.map((value, index) => {
+                            return (
+                                <div className="flex justify-start items-start gap-8">
+                                    <Avatar placeholderInitials={index + 1} rounded></Avatar>
+                                    <div className="flex flex-col gap-1">
+                                        <p className='font-extrabold'>{value.title}</p>
+                                        <p >{value.desc}</p>
+                                    </div>
+
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+
+            <div className="min-h-screen max-w-6xl mx-auto px-6 lg:px-8 pb-10">
+
+                <Card>
+                    <h5 className="text-2xl font-extrabold tracking-tight text-gray-900 dark:text-white">
+                        Form Registrasi Online
+                    </h5>
+
+                    <form >
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="nama" value="Nama Lengkap" />
+                                </div>
+                                <TextInput className="w-full" id="nama" type="text" placeholder="Isi dengan nama lengkap anda" required />
+                            </div>
+
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="email" value="Email" />
+                                </div>
+                                <TextInput className="w-full" id="email" type="email" placeholder="contoh: abc@gmail.com" required />
+                            </div>
+
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="whatsapp" value="Nomor Whatsapp" />
+                                </div>
+                                <TextInput className="w-full" id="whatsapp" type="text" placeholder="contoh: 0821312387xx" required />
+                            </div>
+
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="ttl" value="Tanggal Lahir" />
+                                </div>
+                                <TextInput className="w-full" id="ttl" type="date" placeholder="" required />
+                            </div>
+
+                            <fieldset className="flex max-w-md flex-row gap-4 md:col-span-2">
+                                <legend className="mb-4">Jenis Kelamin</legend>
+                                <div className="flex items-center gap-2">
+                                    <Radio id="united-state" name="countries" value="L" defaultChecked />
+                                    <Label htmlFor="united-state">Laki-Laki</Label>
+                                </div>
+                                <div className="flex items-center gap-2">
+                                    <Radio id="germany" name="countries" value="P" />
+                                    <Label htmlFor="germany">Perempuan</Label>
+                                </div>
+                            </fieldset>
+
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="comment" value="Alamat" />
+                                </div>
+                                <Textarea id="comment" placeholder="Isi Dengan Alamat Lengkap" required rows={4} />
+                            </div>
+
+                            <div id="fileUpload" className="max-w-md">
+                                <div className="mb-2 block">
+                                    <Label htmlFor="file" value="Pas Foto 3x4" />
+                                </div>
+                                <FileInput id="file" helperText="File harus berformat png, jpg, jpeg" />
+                            </div>
+
+                            <div className='md:col-span-2 flex justify-end'>
+                                <Button type="submit">Kirim</Button>
+                            </div>
+                        </div>
+                    </form>
+                </Card>
+            </div>
+
+            <div className=" relative" style={{ backgroundImage: "url(/assets/images/bg-2.png)", backgroundSize: 'cover' }}>
+                <div className='z-10 max-w-6xl mx-auto px-6 lg:px-8 pt-96 md:pt-56 pb-20'>
+                    <div className="grid flex-row-reverse grid-flow-dense lg:grid-cols-3 grid-cols-1 gap-10">
+                        <div className="flex flex-col gap-5">
+                            <img className='w-36' src="assets/images/logo.png" alt="" />
+                            <p className='text-white mt-5'>Pride Chicken adalah usaha mikro kecil menengah dengan sistem kemitraan yang dibangun sejak tahun 2020, Kami membangun usaha ini dengan sistem kemitraan, Kami menghadirkan berbagai menu yang inovatif serta paket kemitraan yang ekonomis.</p>
+                            <div className="flex gap-4 items-center">
+                                <img src="assets/icons/ig.png" alt="" />
+                                <img src="assets/icons/wa.png" alt="" />
+                                <img src="assets/icons/yt.png" alt="" />
+                                <img src="assets/icons/tk.png" alt="" />
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-5">
+                            <p className='font-bold text-xl text-white mt-5'>Hubungi Kami</p>
+                            <div className="flex gap-3 items-center text-white">
+                                <MdEmail fontSize={28} />
+                                <p>kemitraan@pridechicken.com</p>
+                            </div>
+                            <div className="flex gap-3 items-center text-white">
+                                <MdPhone fontSize={28} />
+                                <p>0881 - 0818 - 49988</p>
+                            </div>
+                            <div className="flex gap-3 items-center text-white">
+                                <MdLocationOn fontSize={28} />
+                                <p>Jl. Candra Kirana no. 16, Mekarwangi, Cibaduyut</p>
+                            </div>
+
+                        </div>
+                        <div className="flex flex-col gap-5 text-white">
+                            <p className='font-bold text-xl text-white mt-5'>Info</p>
+                            <p>Kontak</p>
+                            <p>FAQ</p>
+                            <p>Profil Pride Chicken</p>
+                            <p>Download Proposal Kemitraan</p>
+                            <p>Menu</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </MainLayout>
